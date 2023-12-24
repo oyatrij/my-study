28267b9788241a396393ca27e801f3bc49e3bf08
jmlee <oyatrij@gmail.com>
2023-12-24 15:09:33 +0900

    다대다 양방향 연관관계 Member, Team entity 추가

---
+++ JPA/infrean-jpa-basic/projact/jpa-basic/src/main/java/entity/Team.java
@@ -0,0 +1,41 @@
+package entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long Id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<Member>();
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+   }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+}

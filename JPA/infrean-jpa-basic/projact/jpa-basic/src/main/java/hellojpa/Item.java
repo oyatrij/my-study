e0d6a4117c2b4abd012e47d15695cd55a99536df
jmlee <oyatrij@gmail.com>
2023-12-24 15:09:33 +0900

    다대다 양방향 연관관계 Member, Team entity 추가

--- JPA/infrean-jpa-basic/projact/jpa-basic/src/main/java/hellojpa/Item.java
+++ JPA/infrean-jpa-basic/projact/jpa-basic/src/main/java/hellojpa/Item.java
@@ -1,2 +1,47 @@
-package hellojpa;public class Item {
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ITEM_ID")
+public class Item {
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private long itemId;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    public long getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(long itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
 }
--- JPA/infrean-jpa-basic/projact/jpa-basic/src/main/java/hellojpa/Member.java
+++ JPA/infrean-jpa-basic/projact/jpa-basic/src/main/java/hellojpa/Member.java
@@ -1,36 +1,23 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Member {
 
-    @Id
+    @Id @GeneratedValue
     @Column(name="MEMBER_ID")
     private long id;
-    private String username;
+    private String name;
+    private String city;
+    private String street;
+    private String zipcode;
 
-    @ManyToOne
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
-
-    public Member() {
-    }
-
-    public Member(long id, String username) {
-        this.id = id;
-        this.username = username;
-    }
-
-    public void setTeam(Team team) {
-        //팀을 옮겼을 때 이전팀에서 제외시켜준다.
-        if (this.team != null) {
-            this.team.getMembers().remove(this);
-        }
-        this.team = team;
-        team.getMembers().add(this);
-    }
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<Order>();
 
     public long getId() {
         return id;
@@ -40,17 +27,44 @@ public class Member {
         this.id = id;
     }
 
-    public String getUsername() {
-        return username;
+    public String getName() {
+        return name;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Team getTeam() {
-        return team;
+    public String getCity() {
+        return city;
     }
 
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
 

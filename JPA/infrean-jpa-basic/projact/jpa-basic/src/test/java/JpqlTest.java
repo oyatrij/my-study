b271bd39d90aa86601016136e583162d6f7ac7b0
Jaemin Lee <oyatrij@gmail.com>
2024-01-04 08:03:44 +0900

    jpql query exam

--- JPA/infrean-jpa-basic/projact/jpa-basic/src/test/java/JpqlTest.java
+++ JPA/infrean-jpa-basic/projact/jpa-basic/src/test/java/JpqlTest.java
@@ -24,13 +24,32 @@ public class JpqlTest {
     }
 
     @Test
-    public void jpqlTest() {
-        String jpql=  "SELECT m.username, m.age from Member m";
-        List<Member> resultList = em.createQuery(jpql).getResultList();
+    public void jpqlTypequeryTest() {
+        //Typequery
+        //반환 타입이 명확할 경우 사용해야한다.
+        //조회 대상이 Entity일 경우에 사용한다.
+        //조회 대상이 String, Integer 등 Entity가 아닐 경우 Query 를 사용해야한다.
+        String jpql=  "SELECT m from Member m";
+        TypedQuery<Member> query = em.createQuery(jpql, Member.class);
+        List<Member> resultList = query.getResultList();
 
         for (Member member : resultList) {
             System.out.println("username = " + member.getUsername());
             System.out.println("age = " + member.getAge());
+        }
+    }
+
+    @Test
+    public void jpqlQueryTest() {
+        //Query
+        String jpql=  "SELECT m.username, m.age from Member m";
+        Query query = em.createQuery(jpql);
+        List resultList = query.getResultList();
+
+        for (Object o : resultList) {
+            Object[] result = (Object[]) o;
+            System.out.println("username = " + result[0]);
+            System.out.println("age = " + result[1]);
         }
     }
 }

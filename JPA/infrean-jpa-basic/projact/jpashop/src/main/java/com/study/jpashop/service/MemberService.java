964fbf06191640b0b55e3e39ce18f5f0f2d7a24a
Jaemin Lee <oyatrij@gmail.com>
2024-01-08 09:17:40 +0900

    Specification 적용

--- JPA/infrean-jpa-basic/projact/jpashop/src/main/java/com/study/jpashop/service/MemberService.java
+++ JPA/infrean-jpa-basic/projact/jpashop/src/main/java/com/study/jpashop/service/MemberService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -24,8 +25,8 @@ public class MemberService {
         return memberRepository.findAll();
     }
 
-    public Member findOne(Long memberId) {
-        return memberRepository.findOne(memberId);
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 
     private void validateDuplicateMember (Member member) {
